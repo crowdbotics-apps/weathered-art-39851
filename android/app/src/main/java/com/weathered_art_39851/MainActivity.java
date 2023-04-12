0ba7f5cafad94b528b4537d55a2940f24e4f64d9
Crowdbotics <team@crowdbotics.com>
2023-04-12 04:48:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weathered_art_39851/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weathered_art_39851;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weathered_art_39851";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-art-39851",
+  url: "https://weathered-art-39851.botics.co",
+  api: "https://weathered-art-39851.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
